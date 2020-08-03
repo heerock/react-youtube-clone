fc04c286c18e811ed6b70141b6423c1d85e93138
heerock <heerock93@gmail.com>
2020-08-03 19:25:05 +0900

    Add VideoDetailPage && Side Video

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col, List, Avatar } from 'antd';
 import Axios from 'axios';
+import SideVideo from './Sections/SideVideo';
 
 
 function VideoDetailPage(props) {
@@ -43,8 +44,9 @@ function VideoDetailPage(props) {
                     </div>
                 </Col>
                 <Col lg={6} xs={24} >
-                    Side Videos
-                    </Col>
+                    <SideVideo />
+
+                </Col>
             </Row>
         )
 
