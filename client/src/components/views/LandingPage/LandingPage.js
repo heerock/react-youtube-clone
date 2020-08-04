3f52e0f2d0b554f17761f4a3c8685602d1f5b057
heerock <heerock93@gmail.com>
2020-08-04 12:12:37 +0900

    Add Suubscribe

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -56,7 +56,7 @@ function LandingPage() {
 
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
-            <Title level={2} >Recommended</Title>
+            <Title level={2} >Video</Title>
             <hr />
             <Row gutter={[32, 16]}>
                 {renderCards}
