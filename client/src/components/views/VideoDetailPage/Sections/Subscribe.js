7e883492b3c55a564981a47c1f9b527c55780ade
heerock <heerock93@gmail.com>
2020-08-04 12:47:08 +0900

    Subscribe 2

--- client/src/components/views/VideoDetailPage/Sections/Subscribe.js
+++ client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -24,22 +24,59 @@ function Subscribe(props) {
         Axios.post('/api/subscribe/subscribed', subscribedVariable)
             .then(response => {
                 if (response.data.success) {
-                    setSubscribed(response.data.Subscribed)
+                    setSubscribed(response.data.subscribed)
                 } else {
                     alert('정보를 받아오지 못했습니다.')
                 }
             })
     }, [])
 
+
+    const onSubscribe = () => {
+
+        let subscribedVariable = {
+            userTo: props.userTo,
+            userFrom: props.userFrom
+        }
+
+        //이미 구독 중이라면
+        if (Subscribed) {
+            Axios.post('/api/subscribe/unSubscribe', subscribedVariable)
+                .then(response => {
+                    if (response.data.success) {
+                        setSubscribeNumber(SubscribeNumber - 1)
+                        setSubscribed(!Subscribed)
+                    } else {
+                        alert('구독 취소 하는데 실패 했습니다.')
+                    }
+                })
+
+
+            // 아직 구독 중이 아니라면
+        } else {
+            Axios.post('/api/subscribe/subscribe', subscribedVariable)
+                .then(response => {
+                    if (response.data.success) {
+                        setSubscribeNumber(SubscribeNumber + 1)
+                        setSubscribed(!Subscribed)
+
+                    } else {
+                        alert('구독 하는데 실패 했습니다..')
+                    }
+                })
+
+        }
+    }
+
     return (
         <div>
             <button
                 style={{
-                    backgroundColor: `${Subscribe ? '#CC0000' : '#AAAAAA'}`, borderRadius: '4px',
+                    backgroundColor: `${Subscribed ? '#AAAAAA' : '#CC0000'}`, borderRadius: '4px',
                     color: 'white', padding: '10px 16px',
                     fontWeight: '500', fontSize: '1rem', textTransform: 'uppercase'
                 }}
-                onClick
+                onClick={onSubscribe}
             >
                 {SubscribeNumber} {Subscribed ? 'Subscribed' : 'Subscribe'}
             </button>
